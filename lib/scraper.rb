5f1f3cd9547422f9364d7f7bcc5d5a92a3bcb88a
Zoe Keller <keller.zoe98@gmail.com>
2020-06-17 17:43:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,11 @@ require 'nokogiri'
 require 'open-uri'
  
 html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
- 
-courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc = Nokogiri::HTML(htm
+
+
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 
 courses.each do |course|
